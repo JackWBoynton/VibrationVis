51abe5b417bc3b50a2405db8545eada25bf5a1f6
jayceslesar <jslesar@beta.team>
2022-11-15 20:32:06 -0500

    small tweaks

--- src/structure.py
+++ src/structure.py
@@ -15,13 +15,15 @@ class StructureWithSensors:
         width_inches: float,
         height_inches: float,
         sensors: dict[str, tuple[float, float, float]],
+        sensor_colors: dict[str, str],
         density: float = 0.75,
-        points_per_inch: int = 20
+        points_per_inch: int = 25
     ) -> None:
         self.length_inches = length_inches
         self.width_inches = width_inches
         self.height_inches = height_inches
         self.sensors = sensors
+        self.sensor_colors = sensor_colors
         self.density = density
         self.points_per_inch = points_per_inch
 
@@ -48,51 +50,36 @@ class StructureWithSensors:
 
 
     def build_frames(self) -> None:
-        import matplotlib.pyplot as plt
         by_sensors = [self.data[self.data["field"] == sensor] for sensor in self.sensors]
         min_len = min([len(sensor) for sensor in by_sensors])
         trimmed = [sensor[:min_len] for sensor in by_sensors]
 
         frames = []
-        # make me faster
-        for i in tqdm(range(min_len)[5000:50+5000]):
-            # go.Scatter3d(x=self.x, y=self.y, z=self.z, mode="markers", marker=dict(color="lightgrey",opacity=0.5), name="Structure")
-            sensor_plots = []
+
+        for i in tqdm(range(min_len)[:500]):
+            sensor_plots = [go.Scatter3d(x=self.x, y=self.y, z=self.z, mode="markers", name="Structure")]
             for sensor, trimmed_sensor in zip(self.sensors.keys(), trimmed):
                 sensor_x, sensor_y, sensor_z = self.sensors[sensor]
-                propagataion_x = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "X"]["reading"].values[i]) / self.density
-                propagataion_y = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "Y"]["reading"].values[i]) / self.density
-                propagataion_z = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "Z"]["reading"].values[i] + 1) / self.density
-                
-                
-                # get indicies of points within the propagataion radius
-                x_indicies = np.where(np.abs(self.x - sensor_x) < propagataion_x)[0]
-                y_indicies = np.where(np.abs(self.y - sensor_y) < propagataion_y)[0]
-                z_indicies = np.where(np.abs(self.z - sensor_z) < propagataion_z)[0]
-                indicies = np.intersect1d(x_indicies, y_indicies)
-                indicies = np.intersect1d(indicies, z_indicies)
+                propagation_x = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "X"]["reading"].values[i]) / self.density
+                propagation_y = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "Y"]["reading"].values[i]) / self.density
+                propagation_z = np.abs(trimmed_sensor[trimmed_sensor["channel"] == "Z"]["reading"].values[i] + 1) / self.density
 
-                # x_inrange = np.argwhere(np.logical_or(self.x < 4, self.x > 4))
-                x_inrange = np.argwhere(np.abs(self.x - sensor_x) < propagataion_x)
-                y_inrange = np.argwhere(np.logical_or(self.y < sensor_y + propagataion_y, self.y < sensor_y - propagataion_y))
-                z_inrange = np.argwhere(np.logical_or(self.z < sensor_z + propagataion_z, self.z < sensor_z - propagataion_z))
-                print()
-                plt.plot(self.x[x_indicies])
-                plt.show()
-                exit(0)
-                print(len(self.x[x_inrange]), len(self.y[y_inrange]), len(self.z[z_inrange]))
-                sensor_plots.append(go.Scatter3d(x=self.x[x_indices], y=self.y[y_inrange], z=self.z[z_inrange], mode="markers",marker=dict( color="red", opacity=0.9)))
-                # get frame
+                # get indicies of points within the propagation radius
+                x_indicies = np.where(np.abs(self.x - sensor_x) < propagation_x)[0]
+                y_indicies = np.where(np.abs(self.y - sensor_y) < propagation_y)[0]
+                z_indicies = np.where(np.abs(self.z - sensor_z) < propagation_z)[0]
+
+                sensor_plots.append(go.Scatter3d(x=self.x[x_indicies], y=self.y[y_indicies], z=self.z[z_indicies], mode="markers", marker=dict(color=self.sensor_colors[sensor])))
 
             frames.append(go.Frame(data=sensor_plots))
-            # append frame
+
         return frames
 
     def plot(self) -> None:
         fig = go.Figure(
             data=[
                 go.Scatter3d(x=self.x, y=self.y, z=self.z, mode="markers", name="Structure"),
-                *[go.Scatter3d(x=[self.sensors[sensor][0]], y=[self.sensors[sensor][1]], z=[self.sensors[sensor][2]], mode="markers", name=f"Sensor {sensor}") for sensor in self.sensors]
+                *[go.Scatter3d(x=[self.sensors[sensor][0]], y=[self.sensors[sensor][1]], z=[self.sensors[sensor][2]], mode="markers", name=f"Sensor {sensor}", marker=dict(color=self.sensor_colors[sensor])) for sensor in self.sensors]
             ],
             layout=go.Layout(
                 updatemenus=[
@@ -125,7 +112,13 @@ sensors = {
     "B": (0, Y_LEN ,Z_LEN),
     "D": (X_LEN, Y_LEN, Z_LEN),
 }
-my_structure = StructureWithSensors(X_LEN, Y_LEN, Z_LEN, sensors)
+sensor_colors = {
+    "A": "green",
+    "B": "purple",
+    "D": "red",
+}
+
+my_structure = StructureWithSensors(X_LEN, Y_LEN, Z_LEN, sensors, sensor_colors)
 my_structure.load_data("data/test1.csv")
 # my_structure.build_frames()
 my_structure.plot()
