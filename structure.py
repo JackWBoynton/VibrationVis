a0504cc0106f5c25a517310502b331132a6088c8
jayceslesar <jslesar@beta.team>
2022-12-05 18:02:38 -0500

    maybe fix iteration

--- structure.py
+++ structure.py
@@ -144,7 +144,7 @@ class StructureWithSensors:
 
         frames = []
         # make me faster
-        for i in tqdm(range(0, 9203, 100)):
+        for i in tqdm(range(0, 4200, 100)):
             sensor_plots = [go.Scatter3d(x=self.x, y=self.y, z=self.z, mode="markers", marker=dict(color="lightgrey",opacity=0.5), name="Structure")]
             for sensor, trimmed_sensor in zip(self.sensors.keys(), trimmed):
                 sensor_x, sensor_y, sensor_z = self.sensors[sensor]
